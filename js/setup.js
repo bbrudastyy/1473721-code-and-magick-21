6c930df9f0c97f25b1678faed78e2a0d89d45738
Богдан Котов <bbrudastyy@gmail.com>
2020-09-29 04:05:03 +0300

    Исправил ошибки

--- js/setup.js
+++ js/setup.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const WIZARD_NAME = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-const WIZARD_SURNAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-const WIZARD_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-const WIZARD__EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
-const userDialog = document.querySelector('.setup');
-const similarListElement = userDialog.querySelector('.setup-similar-list');
-const similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+const WIZARD_NAME = [`Иван`, `Хуан Себастьян`, `Мария`, `Кристоф`, `Виктор`, `Юлия`, `Люпита`, `Вашингтон`];
+const WIZARD_SURNAME = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
+const WIZARD_COLOR = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
+const WIZARD__EYES_COLOR = [`black`, `red`, `blue`, `yellow`, `green`];
+const userDialog = document.querySelector(`.setup`);
+const similarListElement = userDialog.querySelector(`.setup-similar-list`);
+const similarWizardTemplate = document.querySelector(`#similar-wizard-template`).content.querySelector(`.setup-similar-item`);
 let wizards = [];
 
 function getRandomInt(max) {
@@ -15,7 +15,7 @@ function getRandomInt(max) {
 
 function getWizardInfo() {
   return {
-    name: WIZARD_NAME[getRandomInt(WIZARD_NAME.length)] + ' ' + WIZARD_SURNAME[getRandomInt(WIZARD_SURNAME.length)],
+    name: `${WIZARD_NAME[getRandomInt(WIZARD_NAME.length)]} ${WIZARD_SURNAME[getRandomInt(WIZARD_SURNAME.length)]}`,
     coatColor: WIZARD_COLOR[getRandomInt(WIZARD_COLOR.length)],
     eyesColor: WIZARD__EYES_COLOR[getRandomInt(WIZARD__EYES_COLOR.length)]
   };
@@ -24,9 +24,9 @@ function getWizardInfo() {
 function renderWizard(wizard) {
   const wizardElement = similarWizardTemplate.cloneNode(true);
 
-  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+  wizardElement.querySelector(`.setup-similar-label`).textContent = wizard.name;
+  wizardElement.querySelector(`.wizard-coat`).style.fill = wizard.coatColor;
+  wizardElement.querySelector(`.wizard-eyes`).style.fill = wizard.eyesColor;
 
   return wizardElement;
 }
@@ -40,5 +40,5 @@ for (let i = 0; i < wizards.length; i++) {
   fragment.appendChild(renderWizard(wizards[i]));
 }
 similarListElement.appendChild(fragment);
-userDialog.querySelector('.setup-similar').classList.remove('hidden');
-userDialog.classList.remove('hidden');
+userDialog.querySelector(`.setup-similar`).classList.remove(`hidden`);
+userDialog.classList.remove(`hidden`);
--- js/stat.js
+++ js/stat.js
@@ -9,6 +9,10 @@ const FONT_GAP = 50;
 const BAR_HEIGHT = 150;
 const BAR_WIDTH = 40;
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
 function renderCloud(ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
@@ -26,20 +30,20 @@ function getMaxElement(arr) {
 }
 
 window.renderStatistics = (ctx, names, times) => {
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, `rgba(0, 0, 0, 0.7)`);
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, `#fff`);
 
-  ctx.fillStyle = '#000';
-  ctx.font = '16px PT Mono';
-  ctx.textBaseline = 'hanging';
+  ctx.fillStyle = `#000`;
+  ctx.font = `16px PT Mono`;
+  ctx.textBaseline = `hanging`;
 
-  ctx.fillText("Ура вы победили!", CLOUD_X + GAP * 2, GAP * 2 + GAP / 2);
-  ctx.fillText("Список результатов:", CLOUD_X + GAP * 2, GAP * 4 + GAP / 2);
+  ctx.fillText(`Ура вы победили!`, CLOUD_X + GAP * 2, GAP * 2 + GAP / 2);
+  ctx.fillText(`Список результатов:`, CLOUD_X + GAP * 2, GAP * 4 + GAP / 2);
 
   const maxTime = getMaxElement(times);
 
   for (let i = 0; i < names.length; i++) {
-    ctx.fillStyle = '000000';
+    ctx.fillStyle = `000000`;
     const name = names[i];
     const time = times[i];
 
@@ -52,8 +56,8 @@ window.renderStatistics = (ctx, names, times) => {
     ctx.fillText(name, x, nameY);
     ctx.fillText(Math.floor(time), x, valueY);
     ctx.fillStyle = `hsl(244, ${getRandomInt(100)}% , 44%)`;
-    if (names[i] === "Вы") {
-      ctx.fillStyle = 'hsl(0, 100%, 50%)';
+    if (names[i] === `Вы`) {
+      ctx.fillStyle = `hsl(0, 100%, 50%)`;
     }
     ctx.fillRect(x, barY, BAR_WIDTH, -barHeight);
   }
